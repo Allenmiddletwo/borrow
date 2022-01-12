f576cf56d3239354becd10b32e8038fd402c308c
Allenmiddletwo <10930305@dcsh.tp.edu.tw>
2022-01-12 12:00:22 +0800

    **痾 感謝竑鈞 告訴我大概方向 進度:models 資料模型>借用人

---
+++ log/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+
+class person(models.Model):
+    TITLE_OPTIONS = [
+    (0, '行政人員'),
+    (1, '正式教師'),
+    (2, '代理教師'),
+    (3, '兼任教師'),
+    ]
+
+    title = models.IntegerField(
+              '身分', 
+              default=0, 
+              choices=TITLE_OPTIONS
+           )
+    JOB_OPTIONS = [
+    (0, '國中部'),
+    (1, '高中部'),
+    ] 
+    name = models.CharField('借用人', max_length=10)
+    subject = models.CharField('科目', max_length=10)
+    job_title = models.CharField('職稱', max_length=10)
+    telephone = models.CharField('連絡電話', max_length=10)
+    email = models.CharField('電子郵件', max_length=10)
+    
+    SITUATION_OPTIONS = [
+    (0, '工作中'),
+    (1, '已離職'),
+    ]
+
+    situation = models.IntegerField(
+              '狀態', 
+              default=0, 
+              choices=SITUATION_OPTIONS
+           )
+class number(models.Model):
+    number = models.CharField('設備型號', max_length=10)
